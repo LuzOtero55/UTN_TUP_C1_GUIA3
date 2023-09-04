92f11cfa21ba1503f29ea2aee28551d6a2b6f53f
LuzOtero55 <marialuzoterogrion@hotmail.com>
2023-09-04 01:50:41 -0300

    Create Guia3Recetas.py

---
+++ Guia3Recetas.py
@@ -0,0 +1,8 @@
+print("Consultorio Medico Cruz Roja")
+nombre = input("Ingrese su nombre completo: ")
+fecha = input("Ingrese su fecha de nacimiento: ")
+print("Medicamento Recetado: Plaquenil")
+print("Dosis: 200mg")
+print("Modo de administracion: 1 comprimido por dia")
+print("Av. Los Alcones 1542. Villa Maria")
+
