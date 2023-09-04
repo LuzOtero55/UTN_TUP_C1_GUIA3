72ba54e3ada07d0a550da908ac3cf552b9d7020e
LuzOtero55 <marialuzoterogrion@hotmail.com>
2023-09-04 02:42:54 -0300

    Update Guia3Recetas.py
    Modificación del código anterior con agregado de fecha.

--- Guia3Recetas.py
+++ Guia3Recetas.py
@@ -5,4 +5,13 @@ print("Medicamento Recetado: Plaquenil")
 print("Dosis: 200mg")
 print("Modo de administracion: 1 comprimido por dia")
 print("Av. Los Alcones 1542. Villa Maria")
+""" aqui se modifico el codigo para agregarle la fecha del proximo turno al paciente;
+asi el mismo puede recordarlo facilmente"""
+fechaturno = input("Ingrese la fecha de su Proximo turno:")
+print("Su proximo Turno sera: ",fechaturno,)
+
+
+
+
+
 
